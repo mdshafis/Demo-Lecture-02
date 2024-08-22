48b6de915afbe1485a53103bb328852351b26fb3
SHAFIN <mdshafis@metropolia.fi>
2024-08-22 11:22:47 +0300

    Initial Commit, Variables are defined

--- VariableDemo.py
+++ VariableDemo.py
@@ -1,3 +1,20 @@
+
+Num1 = int(input("Enter a number: "))
+Num2 = float(input("Enter another number: "))
+sum = Num1 + Num2
+print(sum)
+
+name = input("Enter your name: ")
+School = input("Enter your School: ")
+
+print("You are", name, "and your School Name is", School)
+
+print(f"you name is {name} and your school name is {School}")
+floatVariable =4.35698769874
+print(f"Your name is {name} and your float number is : {floatVariable} ")
+print(f"Your name is {name} and your float number is : {floatVariable:.2f} ")
+
+'''
 intVariable = 4
 floatVariable = 4.5
 stringVariable = "Software 1 is fun"
@@ -10,3 +27,16 @@ print(type(intVariable))
 print(type(floatVariable))
 print(type(stringVariable))
 
+#typecastig is fun
+intVariable = int(floatVariable)
+print("Here is the int version of Float Variable ", intVariable)
+'''
+
+'''
+shareOfLoan = 500.50/3
+print(shareOfLoan)
+print(int(shareOfLoan))
+print(type(int(shareOfLoan)))
+
+shareOfLoan = int(shareOfLoan)
+'''
