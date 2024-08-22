da88291e6dfcb2bf298d9bd4bc4a87973aa286eb
SHAFIN <mdshafis@metropolia.fi>
2024-08-22 10:38:41 +0300

    Initial Commit, Variables are defined

---
+++ VariableDemo.py
@@ -0,0 +1,12 @@
+intVariable = 4
+floatVariable = 4.5
+stringVariable = "Software 1 is fun"
+
+print(intVariable)
+print(floatVariable)
+print(stringVariable)
+
+print(type(intVariable))
+print(type(floatVariable))
+print(type(stringVariable))
+
